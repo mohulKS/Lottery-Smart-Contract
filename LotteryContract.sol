9ec69ee5f1a94e85f47de1ad183756e41b55524e
Mohul <mohulkumarshrivastava@gmail.com>
2021-12-28 00:06:38 +0530

    Ok

--- LotteryContract.sol
+++ LotteryContract.sol
@@ -11,7 +11,7 @@ contract Lottery {
     }
 
     function enter() public payable {
-        require(msg.value > 0.01 ether);
+        require(msg.value > 0.02 ether);
 
         players.push(msg.sender);
     }
